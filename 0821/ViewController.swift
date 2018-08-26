96c3927614a96929be15fe203585468bfb1c3798
“Zorro” <ekwzorrowing@gmail.com>
2018-08-26 14:32:27 +0800

    first try

--- 0821/ViewController.swift
+++ 0821/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var zodiacType: UITextField!
+    @IBOutlet weak var genderType: UISegmentedControl!
+    @IBOutlet weak var answerImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func answerSend(_ sender: Any) {
+        if (zodiacType.text)=="leo"{
+            if genderType.selectedSegmentIndex == 1{
+            answerImage.image = UIImage(named: "11")
+        }else{answerImage.image = UIImage(named: "brothers1")
+        }
+    }
+    
+    @IBAction func Closekeyboard(_ sender: Any) {
+    }
 }
-
+}
